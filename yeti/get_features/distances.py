12105d1396a17d03f58fed187553f3d9e6080ecf
Sandro Wrzalek <sandro.wrzalek@creativepython.com>
2019-07-24 11:32:54 +0200

    refactor(Features): distances
    better code maintenance

--- yeti/get_features/distances.py
+++ yeti/get_features/distances.py
@@ -1,6 +1,6 @@
 import numpy as np
-from mdtraj.geometry._geometry import _dist_mic
-from mdtraj.geometry.distance import _distance_mic
+from mdtraj.geometry._geometry import _dist_mic, _dist
+from mdtraj.geometry.distance import _distance_mic, _distance
 from mdtraj.utils.validation import ensure_type
 
 from yeti.get_features.metric import Metric
@@ -8,40 +8,33 @@ from yeti.get_features.metric import Metric
 
 class Distance(Metric):
 
-    def __calculate_no_pbc__(self, atom_01, atom_02):
-        return np.linalg.norm(atom_01.xyz_trajectory - atom_02.xyz_trajectory, axis=1)
+    @staticmethod
+    def __calculate_no_pbc__(xyz, indices, opt):
+        if opt:
+            output = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
+            _dist(xyz, indices, output)
+        else:
+            output = _distance(xyz, indices)
 
-    def __calculate_minimal_image_convention__(self, atom_01, atom_02, opt=True):
-        # create compatibility parameters for mdtraj
-        atom_pairs = np.array([[0, 1]])
+        return output
 
-        xyz_01 = np.expand_dims(atom_01.xyz_trajectory, axis=1)
-        xyz_02 = np.expand_dims(atom_02.xyz_trajectory, axis=1)
-        xyz = np.hstack((xyz_01, xyz_02))
-
-        orthogonal = np.allclose(self.unit_cell_angles, 90)
-
-        # ensure data types are right
-        xyz = ensure_type(xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
+    def __calculate_minimal_image_convention__(self, xyz, indices, opt):
+        # check box
         box = ensure_type(self.unit_cell_vectors, dtype=np.float32, ndim=3, name='unitcell_vectors',
                           shape=(len(xyz), 3, 3), warn_on_cast=False)
-        pairs = ensure_type(atom_pairs, dtype=np.int32, ndim=2, name='atom_pairs', shape=(None, 2), warn_on_cast=False)
+        orthogonal = np.allclose(self.unit_cell_angles, 90)
 
         # calculate distances
         if opt:
-            output = np.empty((xyz.shape[0], 1), dtype=np.float32)
-            _dist_mic(xyz, pairs, box.transpose(0, 2, 1).copy(), output, orthogonal)
+            output = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
+            _dist_mic(xyz, indices, box.transpose(0, 2, 1).copy(), output, orthogonal)
         else:
-            output = _distance_mic(xyz, pairs, box.transpose(0, 2, 1), orthogonal)
+            output = _distance_mic(xyz, indices, box.transpose(0, 2, 1), orthogonal)
 
-        return output.flatten()
+        return output
 
-    def get_distance(self, atom_name_residue_pair_01, atom_name_residue_pair_02):
+    def get_distance(self, atom_name_residue_pair_01, atom_name_residue_pair_02, opt=True):
         name_residue_pairs = (atom_name_residue_pair_01, atom_name_residue_pair_02)
+        atoms = self.__get_atoms__(*name_residue_pairs)
 
-        atom_01, atom_02 = self.__get_atoms__(*name_residue_pairs)
-
-        if self.periodic:
-            return self.__calculate_minimal_image_convention__(atom_01=atom_01, atom_02=atom_02)
-        else:
-            return self.__calculate_no_pbc__(atom_01=atom_01, atom_02=atom_02)
+        return self.__calculate__(atoms=atoms, opt=opt, amount=2)
--- yeti/get_features/metric.py
+++ yeti/get_features/metric.py
@@ -1,4 +1,9 @@
 import numpy as np
+from mdtraj.utils.validation import ensure_type
+
+
+class MetricException(Exception):
+    pass
 
 
 class Metric(object):
@@ -20,4 +25,49 @@ class Metric(object):
         for atom_name, residue in atom_name_residue_pairs:
             atom_list.append(self.__get_atom__(name=atom_name, residue=residue))
 
-        return atom_list
+        return tuple(atom_list)
+
+    @staticmethod
+    def __prepare_xyz_data__(*atoms):
+        xyz = []
+
+        for atom in atoms:
+            tmp_xyz = np.expand_dims(atom.xyz_trajectory, axis=1)
+            xyz.append(tmp_xyz)
+
+        xyz = np.hstack(xyz)
+
+        # ensure data types are right
+        xyz = ensure_type(xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
+
+        return xyz
+
+    @staticmethod
+    def __prepare_atom_indices__(amount):
+        if amount == 2:
+            name = 'atom_pairs'
+            shape = (None, 2)
+        elif amount == 4:
+            name = 'indices'
+            shape = (None, 4)
+        else:
+            raise MetricException('Invalid amount.')
+
+        indices = np.arange(0, amount)
+        indices = np.expand_dims(indices, axis=0)
+        indices = ensure_type(indices, dtype=np.int32, ndim=2, name=name, shape=shape, warn_on_cast=False)
+
+        return indices
+
+    def __calculate__(self, atoms, opt, amount):
+        xyz = self.__prepare_xyz_data__(*atoms)
+        indices = self.__prepare_atom_indices__(amount=amount)
+
+        kwargs = dict(xyz=xyz, indices=indices, opt=opt)
+
+        if self.periodic:
+            feature = self.__calculate_minimal_image_convention__(**kwargs)
+        else:
+            feature = self.__calculate_no_pbc__(**kwargs)
+
+        return feature.flatten()
