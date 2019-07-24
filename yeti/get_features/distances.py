76aab7fb1a88dc50c22e1b94be9af3892c6048ae
Sandro Wrzalek <sandro.wrzalek@creativepython.com>
2019-07-24 14:12:30 +0200

    feat(Dimensions-(aka.-Features)): displacement
    need displacements for dihedral calculation

--- yeti/get_features/distances.py
+++ yeti/get_features/distances.py
@@ -1,6 +1,6 @@
 import numpy as np
-from mdtraj.geometry._geometry import _dist_mic, _dist
-from mdtraj.geometry.distance import _distance_mic, _distance
+from mdtraj.geometry._geometry import _dist_mic, _dist, _dist_mic_displacement, _dist_displacement
+from mdtraj.geometry.distance import _distance_mic, _distance, _displacement_mic, _displacement
 from mdtraj.utils.validation import ensure_type
 
 from yeti.get_features.metric import Metric
@@ -11,12 +11,12 @@ class Distance(Metric):
     @staticmethod
     def __calculate_no_pbc__(xyz, indices, opt):
         if opt:
-            output = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
-            _dist(xyz, indices, output)
+            distances = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
+            _dist(xyz, indices, distances)
         else:
-            output = _distance(xyz, indices)
+            distances = _distance(xyz=xyz, pairs=indices)
 
-        return output
+        return distances
 
     def __calculate_minimal_image_convention__(self, xyz, indices, opt):
         # check box
@@ -26,14 +26,55 @@ class Distance(Metric):
 
         # calculate distances
         if opt:
-            output = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
-            _dist_mic(xyz, indices, box.transpose(0, 2, 1).copy(), output, orthogonal)
+            distances = np.empty((xyz.shape[0], indices.shape[0]), dtype=np.float32)
+
+            _dist_mic(xyz, indices, box.transpose(0, 2, 1).copy(), distances, orthogonal)
         else:
-            output = _distance_mic(xyz, indices, box.transpose(0, 2, 1), orthogonal)
+            distances = _distance_mic(xyz=xyz, pairs=indices, box_vectors=box.transpose(0, 2, 1).copy(),
+                                      orthogonal=orthogonal)
 
-        return output
+        return distances
 
-    def get_distance(self, atom_name_residue_pair_01, atom_name_residue_pair_02, opt=True):
+    def get_distance(self, atom_name_residue_pair_01, atom_name_residue_pair_02, opt=False):
+        name_residue_pairs = (atom_name_residue_pair_01, atom_name_residue_pair_02)
+        atoms = self.__get_atoms__(*name_residue_pairs)
+
+        return self.__calculate__(atoms=atoms, opt=opt, amount=2)
+
+
+class Displacement(Metric):
+    def __calculate_no_pbc__(self, xyz, indices, opt):
+        if opt:
+            displacements = np.empty((xyz.shape[0], indices.shape[0], 3), dtype=np.float32)
+            _dist_displacement(xyz, indices, displacements)
+        else:
+            displacements = _displacement(xyz=xyz, pairs=indices)
+
+        return displacements
+
+    def __calculate_minimal_image_convention__(self, xyz, indices, opt):
+        box = ensure_type(self.unit_cell_vectors, dtype=np.float32, ndim=3, name='unitcell_vectors',
+                          shape=(len(xyz), 3, 3), warn_on_cast=False)
+        orthogonal = np.allclose(self.unit_cell_angles, 90)
+
+        if opt:
+            displacements = np.empty((xyz.shape[0], indices.shape[0], 3), dtype=np.float32)
+            _dist_mic_displacement(xyz, indices, box.transpose(0, 2, 1).copy(), displacements, orthogonal)
+        else:
+            displacements = _displacement_mic(xyz=xyz, pairs=indices, box_vectors=box.transpose(0, 2, 1),
+                                              orthogonal=orthogonal)
+
+        return displacements
+
+    def get_displacement_compatibility_layer(self, xyz, indices, periodic=True, opt=True):
+        kwargs = dict(xyz=xyz, indices=indices, opt=opt)
+
+        if periodic:
+            return self.__calculate_minimal_image_convention__(**kwargs)
+        else:
+            return self.__calculate_no_pbc__(**kwargs)
+
+    def get_displacement(self, atom_name_residue_pair_01, atom_name_residue_pair_02, opt=False):
         name_residue_pairs = (atom_name_residue_pair_01, atom_name_residue_pair_02)
         atoms = self.__get_atoms__(*name_residue_pairs)
 
