cf49c0ffe3c8f4e28c7fdb7cc05ca1e7f9f2613c
Sandro Wrzalek <sandro.wrzalek@creativepython.com>
2019-07-30 14:08:17 +0200

    fix(Systems): residue parameter was misspelled (no breaking change since planned low level API), sequence is now empty list after initialization, method __add_atom__ adds the name of added atom to sequence and adds one to number of atoms, method finalize changes now only types -> atoms list and sequence list become tuple

--- yeti/systems/residue.py
+++ yeti/systems/residue.py
@@ -1,28 +1,23 @@
-import numpy as np
-
-
 class Residue(object):
-    def __init__(self, subssytem_index, structure_file_index, name):
+    def __init__(self, subsystem_index, structure_file_index, name):
         self.atoms = []
+        self.sequence = []
 
         self.name = name
-        self.sequence = None
         self.structure_file_index = structure_file_index
-        self.subssytem_index = subssytem_index
+        self.subsystem_index = subsystem_index
 
-        self.number_of_atoms = len(self.atoms)
+        self.number_of_atoms = 0
 
     def __str__(self):
-        return '{name}{index:d}'.format(name=self.name, index=self.structure_file_index)
+        return '{name}{index:d}'.format(name=self.name, index=self.subsystem_index)
 
     def __add_atom__(self, atom):
         self.atoms.append(atom)
+        self.sequence.append(str(atom))
 
-    def __get_atom_sequence__(self):
-        self.sequence = np.array([str(atom) for atom in self.atoms])
+        self.number_of_atoms += 1
 
     def __finalize__(self):
         self.atoms = tuple(self.atoms)
-        self.number_of_atoms = len(self.atoms)
-
-        self.__get_atom_sequence__()
+        self.sequence = tuple(self.sequence)
