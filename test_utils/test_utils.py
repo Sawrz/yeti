deeb011385d1df30ab0267e28d683fa2432ecfc8
Sandro Wrzalek <sandro.wrzalek@creativepython.com>
2019-08-08 13:54:58 +0200

    test(Utils): test_utils add another atomatic exception message function for easier unit testing

--- test_utils/test_utils.py
+++ test_utils/test_utils.py
@@ -2,6 +2,12 @@ def create_data_type_exception_messages(parameter_name, data_type_name):
     return 'Wrong data type for parameter "{name}". Desired type is {data_type}'.format(name=parameter_name,
                                                                                         data_type=data_type_name)
 
+
 def create_array_shape_exception_messages(parameter_name, desired_shape):
     return 'Wrong shape for parameter "{name}". Desired shape: {des_shape}.'.format(name=parameter_name,
                                                                                     des_shape=desired_shape)
+
+
+def create_array_dtype_exception_messages(parameter_name, dtype_name):
+    return 'Wrong dtype for ndarray "{name}". Desired dtype is {data_type}'.format(name=parameter_name,
+                                                                                   data_type=dtype_name)
