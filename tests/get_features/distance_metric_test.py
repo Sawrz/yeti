1090b94b7b681c8b271cdafd9cb814795264488d
Sandro Wrzalek <sandro.wrzalek@creativepython.com>
2019-10-21 11:47:55 +0200

    test(Dimensions): DistanceMetric refactor tests

--- tests/get_features/distance_metric_test.py
+++ tests/get_features/distance_metric_test.py
@@ -2,41 +2,52 @@ import unittest
 
 import numpy as np
 
-from test_utils.test_utils import create_array_shape_exception_messages
+from tests.blueprints_test import BlueprintTestCase, BlueprintExceptionsTestCase
 
 
-class TestDistanceMetric(unittest.TestCase):
-    def test_init(self):
-        from yeti.get_features.distances import DistanceMetric, DistanceException
-
-        dist = DistanceMetric(periodic=False, unit_cell_angles=np.array([[90, 90, 90]]),
-                              unit_cell_vectors=np.array([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]]))
-
-        self.assertEqual(DistanceException, dist.ensure_data_type.exception_class)
-
-    def test_mdtraj_paramaeter_compatibility_check(self):
+class DistanceMetricTestCase(BlueprintTestCase):
+    def setUp(self) -> None:
         from yeti.get_features.distances import DistanceMetric
 
-        dist = DistanceMetric(periodic=False, unit_cell_angles=np.array([[90, 90, 90]]),
-                              unit_cell_vectors=np.array([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]]))
+        super(DistanceMetricTestCase, self).setUp()
 
-        dist.__mdtraj_paramaeter_compatibility_check__(xyz=np.array([[[0, 0, 0], [1, 0, 0]]], dtype=np.float32),
-                                                       indices=np.array([[0, 1]], dtype=np.int32), opt=True)
+        self.dist = DistanceMetric(unit_cell_angles=np.array([[90, 90, 90]]),
+                                   unit_cell_vectors=np.array([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]]))
 
+        self.xyz = np.array([[[0, 0, 0], [1, 0, 0]]], dtype=np.float32)
+        self.indices = np.array([[0, 1]], dtype=np.int32)
+        self.opt = False
+
+
+class TestStandardMethods(DistanceMetricTestCase):
+    def test_init(self):
+        from yeti.get_features.distances import DistanceException
+
+        self.assertEqual(DistanceException, self.dist.ensure_data_type.exception_class)
+
+
+class TestCompatibilityChecks(DistanceMetricTestCase):
+    def test_mdtraj_paramaeter_compatibility_check(self):
+        self.dist.__mdtraj_paramaeter_compatibility_check__(xyz=self.xyz, indices=self.indices, opt=self.opt)
+
+
+class DihedralExceptionsTestCase(DistanceMetricTestCase, BlueprintExceptionsTestCase):
+    def setUp(self) -> None:
+        from yeti.get_features.distances import DistanceException
+
+        super(DihedralExceptionsTestCase, self).setUp()
+
+        self.exception = DistanceException
+
+
+class TestCompatibilityCheckExceptions(DihedralExceptionsTestCase):
     def test_mdtraj_paramaeter_compatibility_check_xyz_not_match(self):
-        from yeti.get_features.distances import DistanceMetric, DistanceException
-
-        dist = DistanceMetric(periodic=False, unit_cell_angles=np.array([[90, 90, 90]]),
-                              unit_cell_vectors=np.array([[[1, 0, 0], [0, 1, 0], [0, 0, 1]]]))
-
-
-
-        with self.assertRaises(DistanceException) as context:
-            dist.__mdtraj_paramaeter_compatibility_check__(
+        with self.assertRaises(self.exception) as context:
+            self.dist.__mdtraj_paramaeter_compatibility_check__(
                 xyz=np.array([[[0, 0, 0], [1, 0, 0], [1, 0, 0]]], dtype=np.float32),
-                indices=np.array([[0, 1]], dtype=np.int32), opt=True)
+                indices=self.indices, opt=self.opt)
 
-        desired_msg = create_array_shape_exception_messages(parameter_name='xyz', desired_shape=(None, 2, 3))
+        desired_msg = self.create_array_shape_exception_messages(parameter_name='xyz', desired_shape=(None, 2, 3))
         self.assertEqual(desired_msg, str(context.exception))
 
 
