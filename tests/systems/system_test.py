524fe6ac0e59e62d1bb64497f51b9fa31885fd7f
Sandro <sandro.wrzalek@fu-berlin.de>
2019-11-29 11:58:34 +0100

    test(Systems): system add tests for load methods and prepare for streaming capability

--- tests/systems/system_test.py
+++ tests/systems/system_test.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+import mdtraj as md
+import numpy as np
 import numpy.testing as npt
 
 from tests.blueprints_test import BlueprintTestCase
@@ -8,7 +10,6 @@ from tests.blueprints_test import BlueprintTestCase
 
 class SystemTestCase(BlueprintTestCase):
     def setUp(self) -> None:
-        import mdtraj as md
         from yeti.systems.system import System
         from yeti.dictionaries.molecules.biomolecules import RNA
 
@@ -17,28 +18,87 @@ class SystemTestCase(BlueprintTestCase):
         self.gro_file_path = os.path.join(test_data_path, 'AGCUG.gro')
 
         self.reference_trajectory = md.load(filename_or_filenames=self.xtc_file_path, top=self.gro_file_path)
-        self.system = System(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path,
-                             periodic=True)
-
+        self.system = System(name='My System')
         self.molecule_dict = RNA()
 
 
 class TestStandardMethods(SystemTestCase):
     def test_init(self):
-        self.assertEqual(self.system.trajectory_file_path, self.xtc_file_path)
-        self.assertEqual(self.system.topology_file_path, self.gro_file_path)
+        self.assertEqual(self.system.name, 'My System')
 
-        self.assertEqual(self.system.trajectory, self.reference_trajectory)
+        self.assertIsNone(self.system.periodic)
+        self.assertIsNone(self.system.unitcell_angles)
+        self.assertIsNone(self.system.unitcell_vectors)
+        self.assertIsNone(self.system.trajectory)
 
-        # TODO: add arrays instead of references
-        npt.assert_equal(self.system.unitcell_angles, self.reference_trajectory.unitcell_angles)
-        npt.assert_equal(self.system.unitcell_vectors, self.reference_trajectory.unitcell_vectors)
-
-        self.assertEqual(self.system.number_of_frames, 6)
+        self.assertEqual(self.system.number_of_frames, 0)
         self.assertDictEqual(self.system.molecules, {})
 
 
-class TestCreateBuildingBlockMethods(SystemTestCase):
+class TestLoadMethods(SystemTestCase):
+    def setUp(self) -> None:
+        from types import GeneratorType
+        super(TestLoadMethods, self).setUp()
+
+        self.exp_generator = GeneratorType
+        self.exp_number_of_frames = 6
+        self.exp_unit_cell_angles = np.array([[90, 90, 90], [90, 90, 90], [90, 90, 90], [90, 90, 90], [90, 90, 90],
+                                              [90, 90, 90]], dtype=np.float32)
+        self.exp_unit_cell_vectors = np.array(
+            [[[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]], [[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]],
+             [[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]], [[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]],
+             [[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]], [[6.45998, 0, 0], [0, 6.45998, 0], [0, 0, 6.45998]]],
+            dtype=np.float32)
+
+    def test_trajectory_load(self):
+        self.system.__trajectory_load__(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path)
+
+        self.assertEqual(self.system.trajectory, self.reference_trajectory)
+        self.assertEqual(self.system.number_of_frames, self.exp_number_of_frames)
+        npt.assert_almost_equal(self.system.unitcell_angles, self.exp_unit_cell_angles, decimal=5)
+        npt.assert_almost_equal(self.system.unitcell_vectors, self.exp_unit_cell_vectors, decimal=5)
+
+    def test_iter_trajectory_load(self):
+        self.system.__iter_trajectory_load__(trajectory_file_path=self.xtc_file_path,
+                                             topology_file_path=self.gro_file_path, chunk_size=2)
+
+        self.assertEqual(type(self.system.trajectory), self.exp_generator)
+        self.assertEqual(self.system.number_of_frames, self.exp_number_of_frames)
+        npt.assert_almost_equal(self.system.unitcell_angles, self.exp_unit_cell_angles, decimal=5)
+        npt.assert_almost_equal(self.system.unitcell_vectors, self.exp_unit_cell_vectors, decimal=5)
+
+    def test_load_trajectory_no_chunksize(self):
+        self.system.load_trajectory(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path,
+                                    chunk_size=None)
+
+        self.assertEqual(self.system.trajectory, self.reference_trajectory)
+        self.assertEqual(self.system.number_of_frames, self.exp_number_of_frames)
+        npt.assert_almost_equal(self.system.unitcell_angles, self.exp_unit_cell_angles, decimal=5)
+        npt.assert_almost_equal(self.system.unitcell_vectors, self.exp_unit_cell_vectors, decimal=5)
+
+        self.assertTrue(self.system.periodic)
+
+    def test_load_trajectory_with_chunksize(self):
+        self.system.load_trajectory(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path,
+                                    chunk_size=1)
+
+        self.assertEqual(type(self.system.trajectory), self.exp_generator)
+        self.assertEqual(self.system.number_of_frames, self.exp_number_of_frames)
+        npt.assert_almost_equal(self.system.unitcell_angles, self.exp_unit_cell_angles, decimal=5)
+        npt.assert_almost_equal(self.system.unitcell_vectors, self.exp_unit_cell_vectors, decimal=5)
+
+        self.assertTrue(self.system.periodic)
+
+
+class SimpleTrajectoryLoadTestCase(SystemTestCase):
+    def setUp(self) -> None:
+        super(SimpleTrajectoryLoadTestCase, self).setUp()
+
+        self.system.load_trajectory(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path,
+                                    chunk_size=None)
+
+
+class TestCreateBuildingBlockMethodsSimpleLoad(SimpleTrajectoryLoadTestCase):
     def test_create_atom(self):
         atom = self.system.__create_atom__(reference_atom=self.reference_trajectory.topology.atom(10), shifting_index=5)
 
@@ -81,9 +141,9 @@ class TestCreateBuildingBlockMethods(SystemTestCase):
         self.assertEqual(residue.number_of_atoms, 31)
 
 
-class TestCreateBonds(SystemTestCase):
+class TestCreateBondsSimpleLoad(SimpleTrajectoryLoadTestCase):
     def setUp(self) -> None:
-        super(TestCreateBonds, self).setUp()
+        super(TestCreateBondsSimpleLoad, self).setUp()
 
         self.residue_01 = self.system.__create_residue__(
             reference_residue=self.reference_trajectory.topology.residue(0), subsystem_index=0,
@@ -130,9 +190,9 @@ class TestCreateBonds(SystemTestCase):
         self.assertTupleEqual(self.residue_02.atoms[0].covalent_bond_partners, (self.residue_01.atoms[30],))
 
 
-class TestElectronicStates(SystemTestCase):
+class TestElectronicStatesSimpleLoad(SimpleTrajectoryLoadTestCase):
     def setUp(self) -> None:
-        super(TestElectronicStates, self).setUp()
+        super(TestElectronicStatesSimpleLoad, self).setUp()
 
         self.residue = self.system.__create_residue__(reference_residue=self.reference_trajectory.topology.residue(0),
                                                       subsystem_index=0, atom_shifting_index=0)
@@ -172,7 +232,7 @@ class TestElectronicStates(SystemTestCase):
             self.assertEqual(atom.donor_slots, 0)
 
 
-class TestSelectors(SystemTestCase):
+class TestSelectorsSimpleLoad(SimpleTrajectoryLoadTestCase):
     def test_select_residues_is_range(self):
         residues = self.system.__select_residues__(residue_ids=(1, 3), is_range=True)
 
@@ -261,11 +321,12 @@ class TestSelectors(SystemTestCase):
         self.assertEqual(residues[index].atoms[0].subsystem_index, 65)
 
 
-class TestSelectRna(SystemTestCase):
+class TestSelectRnaSimpleLoad(SimpleTrajectoryLoadTestCase):
     def setUp(self) -> None:
-        super(TestSelectRna, self).setUp()
+        super(TestSelectRnaSimpleLoad, self).setUp()
 
-        self.system.select_rna(residue_ids=(0, 1, 4), name='some_virus_rna', distance_cutoff=0.1, angle_cutoff=2.1, is_range=False)
+        self.system.select_rna(residue_ids=(0, 1, 4), name='some_virus_rna', distance_cutoff=0.1, angle_cutoff=2.1,
+                               is_range=False)
 
         self.atoms_res_00 = self.system.molecules['some_virus_rna'].residues[0].atoms
         self.atoms_res_01 = self.system.molecules['some_virus_rna'].residues[1].atoms
@@ -286,7 +347,7 @@ class TestSelectRna(SystemTestCase):
         self.assertTupleEqual(self.atoms_res_00[6].covalent_bond_partners, (self.atoms_res_00[5],))
         self.assertTupleEqual(self.atoms_res_00[7].covalent_bond_partners, (self.atoms_res_00[5], self.atoms_res_00[8]))
         self.assertTupleEqual(self.atoms_res_00[8].covalent_bond_partners, (
-        self.atoms_res_00[7], self.atoms_res_00[9], self.atoms_res_00[26], self.atoms_res_00[10]))
+            self.atoms_res_00[7], self.atoms_res_00[9], self.atoms_res_00[26], self.atoms_res_00[10]))
         self.assertTupleEqual(self.atoms_res_00[9].covalent_bond_partners, (self.atoms_res_00[8],))
 
         # base
@@ -317,10 +378,10 @@ class TestSelectRna(SystemTestCase):
 
         # backbone
         self.assertTupleEqual(self.atoms_res_00[24].covalent_bond_partners, (
-        self.atoms_res_00[5], self.atoms_res_00[25], self.atoms_res_00[26], self.atoms_res_00[30]))
+            self.atoms_res_00[5], self.atoms_res_00[25], self.atoms_res_00[26], self.atoms_res_00[30]))
         self.assertTupleEqual(self.atoms_res_00[25].covalent_bond_partners, (self.atoms_res_00[24],))
         self.assertTupleEqual(self.atoms_res_00[26].covalent_bond_partners, (
-        self.atoms_res_00[8], self.atoms_res_00[24], self.atoms_res_00[27], self.atoms_res_00[28]))
+            self.atoms_res_00[8], self.atoms_res_00[24], self.atoms_res_00[27], self.atoms_res_00[28]))
         self.assertTupleEqual(self.atoms_res_00[27].covalent_bond_partners, (self.atoms_res_00[26],))
         self.assertTupleEqual(self.atoms_res_00[28].covalent_bond_partners,
                               (self.atoms_res_00[26], self.atoms_res_00[29]))
@@ -345,7 +406,7 @@ class TestSelectRna(SystemTestCase):
         self.assertTupleEqual(self.atoms_res_01[9].covalent_bond_partners,
                               (self.atoms_res_01[7], self.atoms_res_01[10]))
         self.assertTupleEqual(self.atoms_res_01[10].covalent_bond_partners, (
-        self.atoms_res_01[9], self.atoms_res_01[11], self.atoms_res_01[29], self.atoms_res_01[12]))
+            self.atoms_res_01[9], self.atoms_res_01[11], self.atoms_res_01[29], self.atoms_res_01[12]))
         self.assertTupleEqual(self.atoms_res_01[11].covalent_bond_partners, (self.atoms_res_01[10],))
 
         # base
@@ -377,10 +438,10 @@ class TestSelectRna(SystemTestCase):
 
         # backbone
         self.assertTupleEqual(self.atoms_res_01[27].covalent_bond_partners, (
-        self.atoms_res_01[7], self.atoms_res_01[28], self.atoms_res_01[29], self.atoms_res_01[33]))
+            self.atoms_res_01[7], self.atoms_res_01[28], self.atoms_res_01[29], self.atoms_res_01[33]))
         self.assertTupleEqual(self.atoms_res_01[28].covalent_bond_partners, (self.atoms_res_01[27],))
         self.assertTupleEqual(self.atoms_res_01[29].covalent_bond_partners, (
-        self.atoms_res_01[10], self.atoms_res_01[27], self.atoms_res_01[30], self.atoms_res_01[31]))
+            self.atoms_res_01[10], self.atoms_res_01[27], self.atoms_res_01[30], self.atoms_res_01[31]))
         self.assertTupleEqual(self.atoms_res_01[30].covalent_bond_partners, (self.atoms_res_01[29],))
         self.assertTupleEqual(self.atoms_res_01[31].covalent_bond_partners,
                               (self.atoms_res_01[29], self.atoms_res_01[32]))
@@ -405,7 +466,7 @@ class TestSelectRna(SystemTestCase):
         self.assertTupleEqual(self.atoms_res_02[9].covalent_bond_partners,
                               (self.atoms_res_02[7], self.atoms_res_02[10]))
         self.assertTupleEqual(self.atoms_res_02[10].covalent_bond_partners, (
-        self.atoms_res_02[9], self.atoms_res_02[11], self.atoms_res_02[29], self.atoms_res_02[12]))
+            self.atoms_res_02[9], self.atoms_res_02[11], self.atoms_res_02[29], self.atoms_res_02[12]))
         self.assertTupleEqual(self.atoms_res_02[11].covalent_bond_partners, (self.atoms_res_02[10],))
 
         # base
@@ -437,10 +498,10 @@ class TestSelectRna(SystemTestCase):
 
         # backbone
         self.assertTupleEqual(self.atoms_res_02[27].covalent_bond_partners, (
-        self.atoms_res_02[7], self.atoms_res_02[28], self.atoms_res_02[29], self.atoms_res_02[33]))
+            self.atoms_res_02[7], self.atoms_res_02[28], self.atoms_res_02[29], self.atoms_res_02[33]))
         self.assertTupleEqual(self.atoms_res_02[28].covalent_bond_partners, (self.atoms_res_02[27],))
         self.assertTupleEqual(self.atoms_res_02[29].covalent_bond_partners, (
-        self.atoms_res_02[10], self.atoms_res_02[27], self.atoms_res_02[30], self.atoms_res_02[31]))
+            self.atoms_res_02[10], self.atoms_res_02[27], self.atoms_res_02[30], self.atoms_res_02[31]))
         self.assertTupleEqual(self.atoms_res_02[30].covalent_bond_partners, (self.atoms_res_02[29],))
         self.assertTupleEqual(self.atoms_res_02[31].covalent_bond_partners,
                               (self.atoms_res_02[29], self.atoms_res_02[32]))
@@ -684,7 +745,36 @@ class TestSelectRna(SystemTestCase):
         from yeti.systems.molecules.nucleic_acids import RNA
 
         self.assertListEqual(list(self.system.molecules.keys()), ['some_virus_rna'])
-        self.assertEqual(self.system.molecules['some_virus_rna'], RNA)
+        self.assertEqual(type(self.system.molecules['some_virus_rna']), RNA)
+
+
+class IterTrajectoryLoadTestCase(SystemTestCase):
+    def setUp(self) -> None:
+        super(IterTrajectoryLoadTestCase, self).setUp()
+
+        self.system.load_trajectory(trajectory_file_path=self.xtc_file_path, topology_file_path=self.gro_file_path,
+                                    chunk_size=4)
+
+
+class TestCreateBuildingBlockMethodsIterLoad(IterTrajectoryLoadTestCase, TestCreateBuildingBlockMethodsSimpleLoad):
+    pass
+
+
+class TestCreateBondsIterLoad(IterTrajectoryLoadTestCase, TestCreateBondsSimpleLoad):
+    pass
+
+
+class TestElectronicStatesIterLoad(IterTrajectoryLoadTestCase, TestElectronicStatesSimpleLoad):
+    pass
+
+
+class TestSelectorsIterLoad(IterTrajectoryLoadTestCase, TestSelectorsSimpleLoad):
+    pass
+
+
+class TestSelectRnaIterLoad(IterTrajectoryLoadTestCase, TestSelectRnaSimpleLoad):
+    pass
+
 
 if __name__ == '__main__':
     unittest.main()
